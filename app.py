e665d17e6f6333232d645b88b4bf5af0d8cbd83e
NI4El <kirillzaidal10@gmail.com>
2023-11-24 00:13:17 +0300

    init project

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('input.html')
+
+
+@app.route('/hello')
+def about():
+    if request.args['name'] == 'Petrovich' or request.args['name'] == 'petrovich':
+        return "Hello," + "lox"
+
+    return "Hello, " + request.args['name']
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
